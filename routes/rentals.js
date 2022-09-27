c2120de716ceef3f14ab0664c5da3454b8083e56
tylerbezona <tylerbezona@gmail.com>
2022-09-27 02:46:02 -0700

    updated rentals

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,4 +1,6 @@
 const { Rental, validate } = require("../models/rental");
+const Customer = require("../models/customer");
+const Movie = require("../models/movie");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
@@ -14,12 +16,31 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send("Invalid Customer");
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send("Invalid Movie");
+
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie no longer in stock.");
+
   let rental = new Rental({
-    name: req.body.name,
-    isGold: req.body.isGold,
-    phone: req.body.phone,
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone,
+    },
+    movie: {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+    },
   });
   rental = await rental.save();
+
+  movie.numberInStock--;
+  movie.save();
 
   res.send(rental);
 });
